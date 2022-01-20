c445a7a4169fb6040e8b34995a87b2ea60fba9d6
Carlo Occhiena <robotfood@virgilio.it>
2022-01-20 12:48:03 +0100

    fix

--- pj_one/app_one/urls.py
+++ pj_one/app_one/urls.py
@@ -4,6 +4,5 @@ from . import views
 # /app_one/
 urlpatterns = [
     path('', views.index, name='index'),
-    path('service_alfa/', views.service_alfa, name='service_alfa'),
-    path('service_beta/', views.service_beta, name='service_beta'),
+    path('<str:service_name>/', views.service_view, name='service_view')
 ]
--- pj_one/app_one/views.py
+++ pj_one/app_one/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # /app_one/
+
+# available services
+
+services = {
+    'service_alfa': 'service_alfa',
+    'service_beta': 'service_beta',
+    'service_gamma': 'service_gamma'
+}
+
+# functions views
+
 def index(request):
     return HttpResponse("Hello, world. You're at the app_one index.")
 
-def service_alfa(request):
-    return HttpResponse("Hello, world. You're at the service_alfa.")
-
-def service_beta(request):
-    return HttpResponse("Hello, world. You're at the service_beta.")
-    
+def service_view(request, service_name):
+    return HttpResponse(f"Hello, world. You're at the {services[service_name]} section.")
