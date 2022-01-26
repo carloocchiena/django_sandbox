8efeec385202d02b14b263d8774866a70eaa40bc
Carlo Occhiena <robotfood@virgilio.it>
2022-01-26 12:42:14 +0100

    update

--- pj_one/first_app/urls.py
+++ pj_one/first_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
+# assign the app namespace
+app_name = 'first_app'
+
 urlpatterns = [
-    path('', views.main_view),
-    path('<int:page_number>', views.page_num_view),
-    path('variables/', views.variables_view),
+    path('', views.main_view, name='app_home'),
+    path('<int:page_number>', views.page_num_view, name='redirect'),
+    path('variables/', views.variables_view, name='variables'),
     path('<str:topic>/', views.news_view, name='topic-page'),
 ]
--- pj_one/pj_one/views.py
+++ pj_one/pj_one/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
 
+
+# probably would make sense to link this to a template within an home_page_app
 def index_view(request):
     return HttpResponse("<h1>Hello World!<h1>")
