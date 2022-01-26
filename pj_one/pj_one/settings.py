31c92eca9a5a7f1bd97cf7904a5b494f81ed80e1
Carlo Occhiena <robotfood@virgilio.it>
2022-01-26 17:20:04 +0100

    update & fix

--- pj_one/pj_one/settings.py
+++ pj_one/pj_one/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y_j=81hmop+!!#w9jt^h%gyyy=+8w8_0+6&97a30h5752&5nm+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
