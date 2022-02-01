de88b9402a97d31cb6b5f8ad393c3708e2410c6d
Carlo Occhiena <robotfood@virgilio.it>
2022-02-01 12:23:28 +0100

    update

--- pj_car/cars/admin.py
+++ pj_car/cars/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from cars.models import Car
 
 # Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    # use fieldsets to group together fields
+    fieldsets = [
+        ('TIME INFORMATION', {'fields': ['year']}),
+        ('NAME', {'fields': ['brand']})
+    ]
+       
+admin.site.register(Car, CarAdmin)
